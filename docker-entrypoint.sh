8e141509c343a99ae5c505cadf8149f312fad94a
Tiago Gois <goistsg@gmail.com>
2024-12-02 19:32:06 -0300

    fix: update to build project

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Espera pelo PostgreSQL
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "postgres" -d "cakecup" -c '\q'; do
+until psql "$DATABASE_URL" -c '\q'; do
   echo "PostgreSQL ainda não está disponível - aguardando..."
   sleep 1
 done
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     uvicorn.run(
         'src.app_module:http_server',
         host="0.0.0.0",
-        port=8000,
+        port=8080,
         reload=True
     )
     
